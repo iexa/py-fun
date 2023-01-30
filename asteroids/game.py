dc4d5f96723d6ec5d720e5ee567b94187ff7917a
CsabaK <898182+iexa@users.noreply.github.com>
2023-01-30 22:16:17 +0100

    bullett shooting added to asteroids

--- asteroids/game.py
+++ asteroids/game.py
@@ -1,14 +1,14 @@
-import pygame
 import logging as log
-from models import GameObj, Ship, Rock, load_sprite
+import pygame
+
+from models import Ship, Rock, load_sprite
 
 
 log.basicConfig(level=log.DEBUG, format='%(levelname)s: %(message)s')
 
 
-
 class Asteroids:
-    
+    """ Main Game """
     def __init__(self):
         pygame.init()
         pygame.display.set_caption('Asteroidzzz')
@@ -17,9 +17,9 @@ class Asteroids:
         self.background = load_sprite('space', False)
         self.collision_count = 0
 
-        self.ship = Ship((400, 300))
+        self.bullets = []
+        self.ship = Ship((400, 300), self.bullets)
         self.rocks = [Rock(self.scr, self.ship.pos) for _ in range(7)]
-
 
     def main_loop(self):
         while 1:
@@ -27,34 +27,41 @@ class Asteroids:
             self._game_logic()
             self._draw()
 
-
     @property
     def game_objects(self):
-        return [*self.rocks, self.ship]
-
+        return [*self.rocks, *self.bullets, self.ship]
 
     def _handle_input(self):
         is_key_pressed = pygame.key.get_pressed()
         # log.info(f"{is_key_pressed=}")
-        for evt in pygame.event.get(): # queue
+        for evt in pygame.event.get():  # queue
             if evt.type == pygame.QUIT or is_key_pressed[pygame.K_ESCAPE]:
                 quit()
+            if evt.type == pygame.KEYDOWN and evt.key == pygame.K_SPACE:
+                self.ship.shoot()
         if is_key_pressed[pygame.K_RIGHT]:
-             self.ship.rotate(clockwise=True)
+            self.ship.rotate(clockwise=True)
         elif is_key_pressed[pygame.K_LEFT]:
             self.ship.rotate(clockwise=False)
         elif is_key_pressed[pygame.K_UP]:
             self.ship.accelerate()
-        
+        elif is_key_pressed[pygame.K_DOWN]:
+            self.ship.decelerate()
+            
 
     def _game_logic(self):
         for obj in self.game_objects:
             obj.move(self.scr)
 
+        # remove bullets outside scr
+        rect = self.scr.get_rect()
+        for bullet in self.bullets[:]:  # cant remove items while iterating over
+            if not rect.collidepoint(bullet.pos):
+                self.bullets.remove(bullet)
 
     def _draw(self):
         # self.scr.fill((0,0,255))
-        self.scr.blit(self.background, (0,0))
+        self.scr.blit(self.background, (0, 0))
         for obj in self.game_objects:
             obj.draw(self.scr)
         pygame.display.flip()
@@ -64,4 +71,4 @@ class Asteroids:
                 self.collision_count += 1
                 log.info(f'{self.collision_count=}')
 
-        self.clock.tick(30) # set framerate
+        self.clock.tick(30)  # set framerate
--- asteroids/models.py
+++ asteroids/models.py
@@ -1,9 +1,10 @@
+from pathlib import Path
+from random import randrange, randint
+
 from pygame.math import Vector2
 from pygame.transform import rotozoom
 from pygame import Surface
 from pygame.image import load
-from pathlib import Path
-from random import randrange, randint
 
 
 DIR_UP = Vector2(0, -1)
@@ -11,61 +12,66 @@ DIR_UP = Vector2(0, -1)
 
 def load_sprite(name: str, with_alpha=True):
     file = Path(__file__).parent / f'assets/{name}.png'
-    sprite = load(file.resolve()) # or file.resolve()
-    if with_alpha:
-        return sprite.convert_alpha()
-    return sprite.convert()
+    sprite = load(file.resolve())  # or file or file.resolve()
+    return sprite.convert_alpha() if with_alpha else sprite.convert()
 
 
 class GameObj:
-    def __init__(self, pos: Vector2, sprite: Surface, velocity):
+    """ base game objects for all sprites """
+    def __init__(self, pos: Vector2, sprite: Surface, velocity, wraps=True):
         self.pos = Vector2(pos)
         self.sprite = sprite
         self.radius = sprite.get_width() / 2
         self.velocity = Vector2(velocity)
+        self.wraps = wraps
 
-    
     def draw(self, surface: Surface):
-        pos = self.pos - Vector2(self.radius) # convert from center to top-left based coord
+        pos = self.pos - Vector2(self.radius)  # convert from center to top-left based coord
         surface.blit(self.sprite, pos)
-
 
     def _wrap_pos(self, surface: Surface):
         x, y = self.pos
         w, h = surface.get_size()
-        return Vector2(x % w, y % h) # simple modulo
-    
-    
+        return Vector2(x % w, y % h)  # simple modulo
+
     def move(self, surface: Surface):
         # surface is on which we move, to be able to wrap around
         self.pos = self.pos + self.velocity
-        self.pos = self._wrap_pos(surface)
-
+        if self.wraps:
+            self.pos = self._wrap_pos(surface)
 
     def collides_with(self, other: 'GameObj'):
         distance = self.pos.distance_to(other.pos)
         return distance < self.radius + other.radius
 
 
-
 class Ship(GameObj):
+    """ ships also shoot bullets """
     ROTATION_SPEED = 3
     ACCELERATION = 0.3
+    BULLET_SPEED = 5
 
-    def __init__(self, pos: Vector2):
+    def __init__(self, pos: Vector2, bullets):
         self.dir = Vector2(DIR_UP)
+        self.bullets = bullets
         super().__init__(pos, load_sprite('spaceship'), Vector2(0))
 
-    
-    def rotate(self, clockwise = True):
+    def rotate(self, clockwise=True):
         sign = 1 if clockwise else -1
         angle = self.ROTATION_SPEED * sign
-        self.dir.rotate_ip(angle) # _ip = in_place
-
+        self.dir.rotate_ip(angle)  # _ip = in_place
 
     def accelerate(self):
         self.velocity += self.dir * self.ACCELERATION
 
+    def decelerate(self):
+        # TODO: check so it cannot go in reverse, so stop at 0 vel.
+        self.velocity -= self.dir * self.ACCELERATION
+
+    def shoot(self):
+        velocity = self.dir * self.BULLET_SPEED + self.velocity
+        bullet = Bullet(self.pos, velocity)
+        self.bullets.append(bullet)
 
     def draw(self, surface: Surface):
         angle = self.dir.angle_to(DIR_UP)
@@ -76,16 +82,15 @@ class Ship(GameObj):
         surface.blit(rotated_surface, blit_pos)
 
 
-
 class Rock(GameObj):
     MIN_START_GAP = 200
     MIN_SPEED = 1
     MAX_SPEED = 3
 
     def __init__(self, surface: Surface, ship_pos: Vector2):
-        while True: # do not be too close to ship
+        while True:  # do not be too close to ship
             pos = Vector2(randrange(surface.get_width()),
-                        randrange(surface.get_height()))
+                          randrange(surface.get_height()))
             if pos.distance_to(ship_pos) > self.MIN_START_GAP:
                 break
 
@@ -94,3 +99,7 @@ class Rock(GameObj):
         velocity = Vector2(speed, 0).rotate(angle)
         super().__init__(pos, load_sprite('asteroid'), velocity)
 
+
+class Bullet(GameObj):
+    def __init__(self, pos: Vector2, velocity):
+        super().__init__(pos, load_sprite('bullet'), velocity, wraps=False)
